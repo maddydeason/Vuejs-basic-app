b935f41d8ac8d1878c01a516dca29e8ee38f79de
Maddy Deason <mdeason@getfused.com>
2017-03-07 15:25:39 -0500

    add vue card, card style, basic markup

---
+++ README.md
@@ -0,0 +1,3 @@
+A basic app made with Vue.js
+View live at http://codepen.io/maddydeason/full/yMNroX/
+ 
---
+++ js/index.js
@@ -0,0 +1,7 @@
+var card = new Vue({
+  el: "#card",
+  data: {
+    title: "The Way of Kings",
+    content: "The Way of Kings is the first book of The Stormlight Archive epic fantasy series written by American author Brandon Sanderson."
+  }
+})
