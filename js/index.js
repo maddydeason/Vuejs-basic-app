dacdba1fc9b1a76772fdad424ab7b65f9293d5e6
Maddy Deason <mdeason89@gmail.com>
2017-03-07 15:37:19 -0500

    set content as v-html attribute

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ var card = new Vue({
   el: "#card",
   data: {
     title: "The Way of Kings",
-    content: "The Way of Kings is the first book of The Stormlight Archive epic fantasy series written by American author Brandon Sanderson."
+    content: "<strong>The Way of Kings</strong> is the first book of The Stormlight Archive epic fantasy series written by American author Brandon Sanderson."
   }
 })
